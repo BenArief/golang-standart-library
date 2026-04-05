8d65fe6bd7b49c95572a0de063a8766512b9f8ef
BenArief <benarief29@gmail.com>
2026-04-05 20:42:45 +0700

    time package

---
+++ time.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	now := time.Now()
+	fmt.Println(now.Local())
+
+	utc := time.Date(2009, time.August, 17, 0, 0, 0, 0, time.UTC)
+	fmt.Println(utc)
+	fmt.Println(utc.Local())
+
+	// time.RFC3339
+	formatter := "2006-01-02 15:04:05"
+	
+	// misal input user
+	value := "2020-10-10 10:10:10"
+	valueTime, err := time.Parse(formatter, value)
+	if err != nil {
+		fmt.Println("Error", err.Error())
+	} else {
+		fmt.Println(valueTime)
+	}
+
+	fmt.Println(valueTime.Year())
+	fmt.Println(valueTime.Month())
+}
